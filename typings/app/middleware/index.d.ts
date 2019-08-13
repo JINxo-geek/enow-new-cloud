09bd20fa09e34a4461206b29a691d88ab4f341a3
严广杰 <yanguangjie@cvte.com>
2019-08-13 07:51:19 +0000

    [modify] modify logger

--- typings/app/middleware/index.d.ts
+++ typings/app/middleware/index.d.ts
@@ -2,7 +2,6 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportAccess from '../../../app/middleware/access';
 import ExportAdaptor from '../../../app/middleware/adaptor';
 import ExportApi from '../../../app/middleware/api';
 import ExportAuth from '../../../app/middleware/auth';
@@ -10,7 +9,6 @@ import ExportUtils from '../../../app/middleware/utils';
 
 declare module 'egg' {
   interface IMiddleware {
-    access: typeof ExportAccess;
     adaptor: typeof ExportAdaptor;
     api: typeof ExportApi;
     auth: typeof ExportAuth;
