8a112bfb4fdccf76bba8bf29bb8ce84b99af23d5
xo JIN <qq527382125@gmail.com@a39ab3d2-397d-4401-a936-3f833362118e>
2019-08-16 11:51:47 +0000

    [fix]修复被覆盖代码

---
+++ app/web/page/home/containers/Content/MyContent/MyContentMsg.tsx
@@ -0,0 +1,58 @@
+const MyContentMsg = [
+  {
+    text: "分享",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "远程演示",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "录制胶囊",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "历史版本",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "Divider"
+  },
+  {
+    text: "重命名",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "移动到",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "新建副本",
+    func: () => {
+      console.log("被点击");
+    }
+  },
+  {
+    text: "Divider"
+  },
+  {
+    text: "删除",
+    func: () => {
+      console.log("被点击");
+    }
+  }
+];
+
+export default MyContentMsg;
