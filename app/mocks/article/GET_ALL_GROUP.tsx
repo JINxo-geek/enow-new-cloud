1438c692fe621c4d3c3cc6832cd7e52e6e4f47a5
xo JIN <qq527382125@gmail.com@a39ab3d2-397d-4401-a936-3f833362118e>
2019-09-04 07:21:44 +0000

    [mock]

---
+++ app/mocks/article/GET_ALL_GROUP.tsx
@@ -0,0 +1,481 @@
+const data = {
+  status: 200,
+  data: [
+    {
+      childrens: [],
+      id: "ecc2ddfb-5113-4238-90e2-b95874fd0e3f",
+      isGroup: true,
+      modifyTime: 1567484294000,
+      name: "测试",
+      parentId: "",
+      serverTime: 1567484294000,
+      treeCode: "",
+      updateTime: 1567484294000
+    },
+    {
+      childrens: [],
+      id: "c8f55b27-998e-48ec-8183-95e8e506e702",
+      isGroup: true,
+      modifyTime: 1566990730000,
+      name: "测试",
+      parentId: "",
+      serverTime: 1566990730000,
+      treeCode: "",
+      updateTime: 1566990729000
+    },
+    {
+      childrens: [
+        {
+          childrens: [
+            {
+              childrens: [
+                {
+                  childrens: [
+                    {
+                      childrens: [
+                        {
+                          childrens: [
+                            {
+                              childrens: [],
+                              id: "aab82a5a-5633-4d7c-b976-a30ca9d312f4",
+                              isGroup: true,
+                              modifyTime: 1567392553000,
+                              name: "新建课件组",
+                              parentId: "1766d15e-6066-4aac-a061-a8b9ae5bf957",
+                              serverTime: 1567392553000,
+                              treeCode:
+                                "daa77f2b-d930-48ec-8afb-a374a8dc7353#51aaf510-f322-435b-9e86-94ac6f352599#8625dcd4-b5fe-4ab5-8f09-6418c330b0f8#d6bcd4a8-12d4-4bab-aea8-2c05ab0b0db0#271af191-850b-4423-898f-006f59f5af3f#1766d15e-6066-4aac-a061-a8b9ae5bf957#",
+                              updateTime: 1567392553000
+                            }
+                          ],
+                          id: "1766d15e-6066-4aac-a061-a8b9ae5bf957",
+                          isGroup: true,
+                          modifyTime: 1567392551000,
+                          name: "新建课件组",
+                          parentId: "271af191-850b-4423-898f-006f59f5af3f",
+                          serverTime: 1567392551000,
+                          treeCode:
+                            "daa77f2b-d930-48ec-8afb-a374a8dc7353#51aaf510-f322-435b-9e86-94ac6f352599#8625dcd4-b5fe-4ab5-8f09-6418c330b0f8#d6bcd4a8-12d4-4bab-aea8-2c05ab0b0db0#271af191-850b-4423-898f-006f59f5af3f#",
+                          updateTime: 1567392551000
+                        }
+                      ],
+                      id: "271af191-850b-4423-898f-006f59f5af3f",
+                      isGroup: true,
+                      modifyTime: 1567392549000,
+                      name: "新建课件组",
+                      parentId: "d6bcd4a8-12d4-4bab-aea8-2c05ab0b0db0",
+                      serverTime: 1567392549000,
+                      treeCode:
+                        "daa77f2b-d930-48ec-8afb-a374a8dc7353#51aaf510-f322-435b-9e86-94ac6f352599#8625dcd4-b5fe-4ab5-8f09-6418c330b0f8#d6bcd4a8-12d4-4bab-aea8-2c05ab0b0db0#",
+                      updateTime: 1567392549000
+                    }
+                  ],
+                  id: "d6bcd4a8-12d4-4bab-aea8-2c05ab0b0db0",
+                  isGroup: true,
+                  modifyTime: 1567392547000,
+                  name: "新建课件组",
+                  parentId: "8625dcd4-b5fe-4ab5-8f09-6418c330b0f8",
+                  serverTime: 1567392547000,
+                  treeCode:
+                    "daa77f2b-d930-48ec-8afb-a374a8dc7353#51aaf510-f322-435b-9e86-94ac6f352599#8625dcd4-b5fe-4ab5-8f09-6418c330b0f8#",
+                  updateTime: 1567392547000
+                }
+              ],
+              id: "8625dcd4-b5fe-4ab5-8f09-6418c330b0f8",
+              isGroup: true,
+              modifyTime: 1566991403000,
+              name: "测试",
+              parentId: "51aaf510-f322-435b-9e86-94ac6f352599",
+              serverTime: 1566991403000,
+              treeCode:
+                "daa77f2b-d930-48ec-8afb-a374a8dc7353#51aaf510-f322-435b-9e86-94ac6f352599#",
+              updateTime: 1566991402000
+            }
+          ],
+          id: "51aaf510-f322-435b-9e86-94ac6f352599",
+          isGroup: true,
+          modifyTime: 1566991392000,
+          name: "测试",
+          parentId: "daa77f2b-d930-48ec-8afb-a374a8dc7353",
+          serverTime: 1566991392000,
+          treeCode: "daa77f2b-d930-48ec-8afb-a374a8dc7353#",
+          updateTime: 1566991391000
+        }
+      ],
+      id: "daa77f2b-d930-48ec-8afb-a374a8dc7353",
+      isGroup: true,
+      modifyTime: 1566987802000,
+      name: "23",
+      parentId: "",
+      serverTime: 1566987802000,
+      treeCode: "",
+      updateTime: 1566987802000
+    },
+    {
+      childrens: [],
+      id: "58938b2b-4bb9-4280-8955-2bfcf70e0c7e",
+      isGroup: true,
+      modifyTime: 1566869014000,
+      name: "开小灶-时间胶囊（请勿删改测试课件）",
+      parentId: "",
+      serverTime: 1548057957000,
+      treeCode: "",
+      updateTime: 1566869005000
+    },
+    {
+      childrens: [],
+      id: "dcf9778f-d14a-4580-a48e-b6cb5e641044",
+      isGroup: true,
+      modifyTime: 1565230155000,
+      name: "8A A2送测",
+      parentId: "",
+      serverTime: 1565230138000,
+      treeCode: "",
+      updateTime: 1565230155000
+    },
+    {
+      childrens: [],
+      id: "b34b61f1-bf3f-4368-bbfc-c38062467354",
+      isGroup: true,
+      modifyTime: 1562209288000,
+      name: "梁晓华_ENOW测试课件（请勿删改测试课件）",
+      parentId: "",
+      serverTime: 1560130316000,
+      treeCode: "",
+      updateTime: 1562209249000
+    },
+    {
+      childrens: [],
+      id: "1f56c1ee-3bb0-40b5-8cfc-3dbd4b20ad49",
+      isGroup: true,
+      modifyTime: 1558332053000,
+      name: "ENOW 相关方上报bug",
+      parentId: "",
+      serverTime: 1554973051000,
+      treeCode: "",
+      updateTime: 1558332047000
+    },
+    {
+      childrens: [],
+      id: "f23fa7b5-ed75-462c-90af-0096096c8f86",
+      isGroup: true,
+      modifyTime: 1554360598000,
+      name: "只读课件",
+      parentId: "",
+      serverTime: 1554346450000,
+      treeCode: "",
+      updateTime: 1554360597000
+    },
+    {
+      childrens: [],
+      id: "27366775-13d4-4918-806e-74e66c69af21",
+      isGroup: true,
+      modifyTime: 1552711136000,
+      name: "方法",
+      parentId: "",
+      serverTime: 1552711130000,
+      treeCode: "",
+      updateTime: 1552711137000
+    },
+    {
+      childrens: [],
+      id: "cce8836b-af97-4284-8937-7ec868f6e7bd",
+      isGroup: true,
+      modifyTime: 1552637404000,
+      name:
+        "岩心你空间看了可能是都可能分开吗卢克吗卢克吗拉开门离开卢克吗卢克吗；路口",
+      parentId: "",
+      serverTime: 1552637385000,
+      treeCode: "",
+      updateTime: 1552637406000
+    },
+    {
+      childrens: [
+        {
+          childrens: [],
+          id: "f00a1d55-1d12-4d08-8363-87e8d09d0189",
+          isGroup: true,
+          modifyTime: 1552096581000,
+          name: "新建课件组",
+          parentId: "54169524-90d8-471f-a110-d15eb975ac83",
+          serverTime: 1552096581000,
+          treeCode: "54169524-90d8-471f-a110-d15eb975ac83#",
+          updateTime: 1552096583000
+        }
+      ],
+      id: "54169524-90d8-471f-a110-d15eb975ac83",
+      isGroup: true,
+      modifyTime: 1551255128000,
+      name: "新建分享课件组",
+      parentId: "",
+      serverTime: 1551255120000,
+      treeCode: "",
+      updateTime: 1551255128000
+    },
+    {
+      childrens: [
+        {
+          childrens: [
+            {
+              childrens: [],
+              id: "9a64713d-a625-487b-8aa5-dfce27b93269",
+              isGroup: true,
+              modifyTime: 1567138069000,
+              name: "213213",
+              parentId: "725c81d2-d665-4b86-a4e7-65822b10c35d",
+              serverTime: 1567138069000,
+              treeCode:
+                "5c35d61b-df45-4d38-b865-d8bbcd0d08a9#725c81d2-d665-4b86-a4e7-65822b10c35d#",
+              updateTime: 1567138069000
+            }
+          ],
+          id: "725c81d2-d665-4b86-a4e7-65822b10c35d",
+          isGroup: true,
+          modifyTime: 1548464852000,
+          name: "环境",
+          parentId: "5c35d61b-df45-4d38-b865-d8bbcd0d08a9",
+          serverTime: 1547032897000,
+          treeCode: "5c35d61b-df45-4d38-b865-d8bbcd0d08a9#",
+          updateTime: 1547032902000
+        }
+      ],
+      id: "5c35d61b-df45-4d38-b865-d8bbcd0d08a9",
+      isGroup: true,
+      modifyTime: 1548919147000,
+      name: "七层课件组",
+      parentId: "",
+      serverTime: 1543216358000,
+      treeCode: "",
+      updateTime: 1548919148000
+    },
+    {
+      childrens: [
+        {
+          childrens: [],
+          id: "b98a2ff9-bee7-4442-90ee-7ceb9a0d6a50",
+          isGroup: true,
+          modifyTime: 1552390159000,
+          name: "发布版本课件",
+          parentId: "83732015-ec76-4f83-95c7-03df668103d4",
+          serverTime: 1528334702000,
+          treeCode: "83732015-ec76-4f83-95c7-03df668103d4#",
+          updateTime: 1528334705000
+        }
+      ],
+      id: "83732015-ec76-4f83-95c7-03df668103d4",
+      isGroup: true,
+      modifyTime: 1553509452000,
+      name: "ENOW开发课件",
+      parentId: "",
+      serverTime: 1547698067000,
+      treeCode: "",
+      updateTime: 1547698083000
+    },
+    {
+      childrens: [
+        {
+          childrens: [],
+          id: "4ce067cf-86e9-4829-a8c4-62bde6e654f6",
+          isGroup: true,
+          modifyTime: 1567138605000,
+          name: "ww",
+          parentId: "33fbffc6-a1b3-468c-b39a-3fd950201263",
+          serverTime: 1567138605000,
+          treeCode: "33fbffc6-a1b3-468c-b39a-3fd950201263#",
+          updateTime: 1567138605000
+        },
+        {
+          childrens: [
+            {
+              childrens: [
+                {
+                  childrens: [
+                    {
+                      childrens: [
+                        {
+                          childrens: [
+                            {
+                              childrens: [],
+                              id: "e532c0c8-fd1d-4d3b-9fcd-134cd18c50af",
+                              isGroup: true,
+                              modifyTime: 1563851041000,
+                              name: "六",
+                              parentId: "e199d1dc-0ba2-413e-950c-bb43a2a591a3",
+                              serverTime: 1563851035000,
+                              treeCode:
+                                "33fbffc6-a1b3-468c-b39a-3fd950201263#efdf96f1-380d-4d21-86f2-2bec99c989a8#985f49f4-05f7-43d6-8dbb-5381a7ef11b1#a3d81ff8-65f6-48f8-9ae2-e6fce13b8bef#90564bb4-0ab3-4b86-bba4-7136ff7de0c5#e199d1dc-0ba2-413e-950c-bb43a2a591a3#",
+                              updateTime: 1563850942000
+                            }
+                          ],
+                          id: "e199d1dc-0ba2-413e-950c-bb43a2a591a3",
+                          isGroup: true,
+                          modifyTime: 1563851032000,
+                          name: "五",
+                          parentId: "90564bb4-0ab3-4b86-bba4-7136ff7de0c5",
+                          serverTime: 1563851020000,
+                          treeCode:
+                            "33fbffc6-a1b3-468c-b39a-3fd950201263#efdf96f1-380d-4d21-86f2-2bec99c989a8#985f49f4-05f7-43d6-8dbb-5381a7ef11b1#a3d81ff8-65f6-48f8-9ae2-e6fce13b8bef#90564bb4-0ab3-4b86-bba4-7136ff7de0c5#",
+                          updateTime: 1563850932000
+                        }
+                      ],
+                      id: "90564bb4-0ab3-4b86-bba4-7136ff7de0c5",
+                      isGroup: true,
+                      modifyTime: 1563851018000,
+                      name: "四",
+                      parentId: "a3d81ff8-65f6-48f8-9ae2-e6fce13b8bef",
+                      serverTime: 1563851015000,
+                      treeCode:
+                        "33fbffc6-a1b3-468c-b39a-3fd950201263#efdf96f1-380d-4d21-86f2-2bec99c989a8#985f49f4-05f7-43d6-8dbb-5381a7ef11b1#a3d81ff8-65f6-48f8-9ae2-e6fce13b8bef#",
+                      updateTime: 1563850918000
+                    }
+                  ],
+                  id: "a3d81ff8-65f6-48f8-9ae2-e6fce13b8bef",
+                  isGroup: true,
+                  modifyTime: 1563851009000,
+                  name: "三",
+                  parentId: "985f49f4-05f7-43d6-8dbb-5381a7ef11b1",
+                  serverTime: 1563851006000,
+                  treeCode:
+                    "33fbffc6-a1b3-468c-b39a-3fd950201263#efdf96f1-380d-4d21-86f2-2bec99c989a8#985f49f4-05f7-43d6-8dbb-5381a7ef11b1#",
+                  updateTime: 1563850910000
+                }
+              ],
+              id: "985f49f4-05f7-43d6-8dbb-5381a7ef11b1",
+              isGroup: true,
+              modifyTime: 1563851004000,
+              name: "二",
+              parentId: "efdf96f1-380d-4d21-86f2-2bec99c989a8",
+              serverTime: 1563851001000,
+              treeCode:
+                "33fbffc6-a1b3-468c-b39a-3fd950201263#efdf96f1-380d-4d21-86f2-2bec99c989a8#",
+              updateTime: 1563850904000
+            }
+          ],
+          id: "efdf96f1-380d-4d21-86f2-2bec99c989a8",
+          isGroup: true,
+          modifyTime: 1563850998000,
+          name: "测试多层课件组",
+          parentId: "33fbffc6-a1b3-468c-b39a-3fd950201263",
+          serverTime: 1563850980000,
+          treeCode: "33fbffc6-a1b3-468c-b39a-3fd950201263#",
+          updateTime: 1563850898000
+        },
+        {
+          childrens: [],
+          id: "64703cf2-867a-4913-a59b-632a2b5f3faf",
+          isGroup: true,
+          modifyTime: 1554708374000,
+          name: "动画重构测试集合",
+          parentId: "33fbffc6-a1b3-468c-b39a-3fd950201263",
+          serverTime: 1554708359000,
+          treeCode: "33fbffc6-a1b3-468c-b39a-3fd950201263#",
+          updateTime: 1554708381000
+        },
+        {
+          childrens: [
+            {
+              childrens: [],
+              id: "76ee64e0-15c1-40cd-91b4-6565e1efb755",
+              isGroup: true,
+              modifyTime: 1554708375000,
+              name: "验证课件大小对学务推送的影响",
+              parentId: "fdcbc6bb-c8f3-41fb-966e-bdb48438f5c3",
+              serverTime: 1553507644000,
+              treeCode:
+                "33fbffc6-a1b3-468c-b39a-3fd950201263#fdcbc6bb-c8f3-41fb-966e-bdb48438f5c3#",
+              updateTime: 1553507716000
+            },
+            {
+              childrens: [],
+              id: "6e59aa86-38b5-4c70-8316-b6bbc4d8473c",
+              isGroup: true,
+              modifyTime: 1552390156000,
+              name: "冠军课件2",
+              parentId: "fdcbc6bb-c8f3-41fb-966e-bdb48438f5c3",
+              serverTime: 1536889298000,
+              treeCode:
+                "33fbffc6-a1b3-468c-b39a-3fd950201263#fdcbc6bb-c8f3-41fb-966e-bdb48438f5c3#",
+              updateTime: 1536889298000
+            }
+          ],
+          id: "fdcbc6bb-c8f3-41fb-966e-bdb48438f5c3",
+          isGroup: true,
+          modifyTime: 1554118030000,
+          name: "PPT课件",
+          parentId: "33fbffc6-a1b3-468c-b39a-3fd950201263",
+          serverTime: 1550106756000,
+          treeCode: "33fbffc6-a1b3-468c-b39a-3fd950201263#",
+          updateTime: 1550106766000
+        }
+      ],
+      id: "33fbffc6-a1b3-468c-b39a-3fd950201263",
+      isGroup: true,
+      modifyTime: 1547688015000,
+      name: "Enow测试课件（请勿删改测试课件）",
+      parentId: "",
+      serverTime: 1521623815000,
+      treeCode: "",
+      updateTime: 1547688015000
+    },
+    {
+      childrens: [
+        {
+          childrens: [],
+          id: "a4ba8fcf-f510-4c56-a511-c9dcfcc7f56a",
+          isGroup: true,
+          modifyTime: 1567146690000,
+          name: "测试",
+          parentId: "6bb63711-92c9-4758-be54-1f217a1ecc0b",
+          serverTime: 1567146690000,
+          treeCode: "6bb63711-92c9-4758-be54-1f217a1ecc0b#",
+          updateTime: 1567146690000
+        },
+        {
+          childrens: [],
+          id: "0cf98ab9-e816-476d-be42-96e0829cb1bc",
+          isGroup: true,
+          modifyTime: 1552876220000,
+          name: "个多少个",
+          parentId: "6bb63711-92c9-4758-be54-1f217a1ecc0b",
+          serverTime: 1552876211000,
+          treeCode: "6bb63711-92c9-4758-be54-1f217a1ecc0b#",
+          updateTime: 1552876222000
+        },
+        {
+          childrens: [
+            {
+              childrens: [],
+              id: "df654112-b8f2-4bc0-ace1-911be9c808b4",
+              isGroup: true,
+              modifyTime: 1567146683000,
+              name: "测试",
+              parentId: "50cf8f07-bdca-4a7f-9db0-1472c8dfd3a1",
+              serverTime: 1567146683000,
+              treeCode:
+                "6bb63711-92c9-4758-be54-1f217a1ecc0b#50cf8f07-bdca-4a7f-9db0-1472c8dfd3a1#",
+              updateTime: 1567146683000
+            }
+          ],
+          id: "50cf8f07-bdca-4a7f-9db0-1472c8dfd3a1",
+          isGroup: true,
+          modifyTime: 1565701780000,
+          name: "动画用例执行",
+          parentId: "6bb63711-92c9-4758-be54-1f217a1ecc0b",
+          serverTime: 1545202468000,
+          treeCode: "6bb63711-92c9-4758-be54-1f217a1ecc0b#",
+          updateTime: 1545202490000
+        }
+      ],
+      id: "6bb63711-92c9-4758-be54-1f217a1ecc0b",
+      isGroup: true,
+      modifyTime: 1528786957000,
+      name: "其他",
+      parentId: "",
+      serverTime: 1528786947000,
+      treeCode: "",
+      updateTime: 1528786960000
+    }
+  ],
+  error_code: 0,
+  message: "ok"
+};
---
+++ app/mocks/article/GET_RECIVE.tsx
@@ -0,0 +1,23 @@
+const data = {
+  status: 200,
+  data: {
+    content: [
+      {
+        author: "(•౪• )",
+        coursewareCount: 1,
+        coursewareId: "4d0430b5-4e1d-456f-9af2-0991669334ed",
+        coursewareUpdateTime: 1567498211000,
+        createTime: 1567578315000,
+        name: "未命名课件 20190729-1012",
+        size: 9831289,
+        type: 1,
+        uid: "b438762c97d2446dbe13849ba0265199",
+        updateTime: 1567578315000
+      }
+    ],
+    index: 0,
+    total: 1
+  },
+  error_code: 0,
+  message: "ok"
+};
