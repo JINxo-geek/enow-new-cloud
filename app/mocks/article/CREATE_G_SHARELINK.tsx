dc3d125a800115f28c870f660d6aba4bbc50f349
xo JIN <qq527382125@gmail.com>
2019-09-04 07:21:44 +0000

    [mock]

---
+++ app/mocks/article/CREATE_G_SHARELINK.tsx
@@ -0,0 +1,11 @@
+const data = {
+  status: 200,
+  data: {
+    content: "存储测试课件-文本(2018)",
+    link: "https://r302.cc/xOwVNe",
+    password: "",
+    shareLinkUid: "d01b59f2838446669bdfe8ea66fd7856"
+  },
+  error_code: 0,
+  message: "ok"
+};
